887df829e9bcf444639c087bb7e1cf2277f2ed02
vega2004 <kevinvega27100414@gmail.com>
2026-07-20 23:48:00 -0600

    test: add unit integration and security test projects

---
+++ IntegrationTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegrationTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ SecurityTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SecurityTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
