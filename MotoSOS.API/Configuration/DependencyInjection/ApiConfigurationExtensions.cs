8f300882f816c2b4e13d18c79d3daa113f3bc84e
vega2004 <kevinvega27100414@gmail.com>
2026-08-04 22:31:33 -0600

    Subiendo cambios de configuracion de cors

--- MotoSOS.API/Configuration/DependencyInjection/ApiConfigurationExtensions.cs
+++ MotoSOS.API/Configuration/DependencyInjection/ApiConfigurationExtensions.cs
@@ -2,10 +2,24 @@ namespace MotoSOS.API.Configuration.DependencyInjection;
 
 public static class ApiConfigurationExtensions
 {
-    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    public const string CorsPolicyName = "MotoSosCorsPolicy";
+
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddControllers();
         services.AddOpenApi();
+        services.AddCors(options =>
+        {
+            options.AddPolicy(CorsPolicyName, policy =>
+            {
+                string[] allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
+
+                policy
+                    .WithOrigins(allowedOrigins)
+                    .WithHeaders("Authorization", "Content-Type")
+                    .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+            });
+        });
 
         return services;
     }
